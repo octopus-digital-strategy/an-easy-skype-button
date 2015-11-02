0500ce910620c11445aef162a05ff431c264c244
awesomepage <awesome@pagecarbajal.com>
2015-11-02 15:00:11 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,18 @@
+#An Easy Skype Button
+
+This plugin registers the shortcode **an-easy-skype-button** to be used with WordPress
+ 
+The shortcode receives 1 parameter **account** where you specify the name of the account you want to call.
+ 
+
+##Features
+
+* Use Skype scripts to create the button
+* Settings Page to specify default account
+
+
+###Version 0.1
+
+* Tag to 0.1
+* Added the SettingsPage class
+* Added the Shortcodes class
---
+++ init.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Plugin name: An Easy Skype Button
+ * Plugin URI: https://github.com/octopus-digital-strategy/an-easy-skype-button
+ * Description: A really simple plugin to create a Skype button with a short code.
+ * Version: 0.1
+ * Author: Page-Carbajal
+ * Author URI: http://pagecarbajal.com
+ * Tags: bare-bones-plugin, wp-express
+ */
+
+// No direct access to this file
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+// Composer implementation
+require_once('vendor/autoload.php');
+
+// Instance the Setup
+new \AnEasySkypeButton\Setup();
